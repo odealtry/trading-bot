f044dd7d61b43cbaaeb581b2a7d5af2c59fff26b
Oscar Dealtry <odealtry@gmail.com>
2021-02-08 13:38:30 +0000

    cleaning directory

--- pull.py
+++ pull.py
@@ -2,14 +2,15 @@ import pandas
 from alpha_vantage.timeseries import TimeSeries
 import sys
 import random
+import config
 
 ticker = str(sys.argv[1])
 
 api_key = open('.gitignore').read().splitlines()[0]
 
-time = TimeSeries(key=api_key, output_format='pandas')
+time = TimeSeries(key=config.AlphaVantageAPIKey, output_format='pandas')
 
-data = time.get_intraday(symbol=ticker, interval='1min', outputsize='full');
+data = time.get_intraday(symbol=ticker, interval='30min', outputsize='full');
 
 print(ticker)
 print(data)
