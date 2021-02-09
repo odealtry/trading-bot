6c23989fce8c66b21d81fb66a08011f0ce8f983a
Oscar Dealtry <odealtry@gmail.com>
2021-02-09 12:49:23 +0000

    calculating simple moving average of given ticker

--- pull.py
+++ pull.py
@@ -1,16 +1,32 @@
-import pandas
+import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
 import sys
 import random
 import config
 
 ticker = str(sys.argv[1])
 
-api_key = open('.gitignore').read().splitlines()[0]
+api_key = config.AlphaVantageAPIKey
 
-time = TimeSeries(key=config.AlphaVantageAPIKey, output_format='pandas')
+ts = TimeSeries(key=api_key, output_format='pandas')
 
-data = time.get_intraday(symbol=ticker, interval='30min', outputsize='full');
+data, meta_data = ts.get_intraday(symbol=ticker, interval='60min', outputsize='full');
 
-print(ticker)
-print(data)
+# print(ticker)
+# print(meta_data)
+# print(data)
+
+# calculating simple moving average from a period of 20 days
+period = 480
+
+ti = TechIndicators(key=api_key, output_format='pandas')
+
+ti_data, ti_meta_data = ti.get_sma(symbol=ticker, interval='60min',
+                        time_period=period, series_type='close')
+
+df1 = ti_data
+df2 = data['4. close']
+
+print(df1)
+print(df2)
