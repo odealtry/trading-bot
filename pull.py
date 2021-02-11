076a93458ac92fe8e39de9a6c8c79801b20107fd
Oscar Dealtry <odealtry@gmail.com>
2021-02-11 13:17:01 +0000

    need to manually adjust historic closes and highs according to the split coefficient

--- pull.py
+++ pull.py
@@ -20,9 +20,8 @@ data, meta_data = ts.get_daily_adjusted(symbol=ticker, outputsize='full');
 # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 #     print(data)
 
-# calculating simple moving average from a period of 50 days.
-# this period will be made dynamic by the information calculated
-# in volatility_calculation.py
+# calculating simple moving average from a period of 50 days
+# (seemingly industry standard)
 
 period = 50
 
@@ -33,18 +32,22 @@ ti_data, ti_meta_data = ti.get_sma(symbol=ticker, interval='daily',
 
 # reversing df1 row order and equalising df sizes:
 df1 = ti_data.iloc[::-1]
-df2 = data['5. adjusted close'].iloc[:-(period-1)]
+df2 = data['4. close'].iloc[:-(period-1)]
 df3 = data['2. high'].iloc[:-(period-1)]
+split_coefficient_df = data['8. split coefficient'].iloc[:-(period-1)]
 
-df1.index = df2.index = df3.index
+df1.index = df2.index = df3.index = split_coefficient_df.index
 
-concatenated_df = pd.concat([df1, df2, df3], axis=1)
+concatenated_df = pd.concat([df1, df2, df3, split_coefficient_df], axis=1)
+
+# need to find the moment where the split coefficient != 1,
+# then adjust data prior to that entry according to its value.
 
 
-# reducing dataframe to manageable size:
-closes_with_sma = concatenated_df[:100]
+# reducing dataframe to annual size:
+# closes_with_sma = concatenated_df[:251]
 
 # closes_with_sma = pd.read_json('AAPL_data.json')
 
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print(closes_with_sma)
+    # print(closes_with_sma)
