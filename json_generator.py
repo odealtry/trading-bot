01aac19b8108d04b909d3f4383deddd29e8f8962
Oscar Dealtry <odealtry@gmail.com>
2021-02-15 16:45:22 +0000

    directory restructuring

--- json_generator.py
+++ json_generator.py
@@ -3,6 +3,6 @@ import intraday_pull
 import pandas as pd
 import sys
 
-buy_and_hold_pull.buy_and_hold_data.to_json(path_or_buf='buy_and_hold_data.json')
+buy_and_hold_pull.buy_and_hold_data.to_json(path_or_buf='data/buy_and_hold_data.json')
 
-intraday_pull.intraday_data.to_json(path_or_buf='intraday_data.json')
+intraday_pull.intraday_data.to_json(path_or_buf='data/intraday_data.json')
