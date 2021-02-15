1f598bdd2162abb7b84a82a6a30189fb50595688
Oscar Dealtry <odealtry@gmail.com>
2021-02-15 22:47:02 +0000

    pseudocode for adding stock to holdings

--- buy_and_hold_algorithm.py
+++ buy_and_hold_algorithm.py
@@ -4,8 +4,6 @@ buy_and_hold_data = pd.read_json('data/buy_and_hold_data.json')
 
 # todo: implement logic that finds and saves the maximum value
 # from the 'high' column and triggers a buy signal once it is exceeded.
-# this could be done by creating a class with an instance variable
-# 'purchase' set to false
 # then
 
 print(buy_and_hold_data)
@@ -14,3 +12,9 @@ max_entry = buy_and_hold_data.loc[buy_and_hold_data['4. close'].idxmax()]
 
 max_close = max_entry['4. close']
 
+# compare today's to all-time.
+# if today's is highest, check if the stock is already in holdings.
+# if it is not, trigger the buy method of a new purchase file,
+# passing the stock ticker as an arg.
+# the logic for logging this purchase in holdings
+# can go in a separate file.
