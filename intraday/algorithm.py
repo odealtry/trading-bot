52fcec0ae97a3b2ec791a2ff30b5a0475acfd35a
Oscar Dealtry <odealtry@gmail.com>
2021-03-08 16:24:27 +0000

    class conversion process finished and stop loss introduced, now need to set about refining buy and sell logic

--- intraday/algorithm.py
+++ intraday/algorithm.py
@@ -5,57 +5,65 @@ intraday_data = pd.read_json('intraday/data/data.json')
 intraday_data['buy'] = False
 intraday_data['sell'] = False
 
-print(intraday_data)
-
-# Algorithm class should have an evaluate function that:
-# iterates through the datatset;
-# redirects depending on whether pos == 0;
-# recreates buy and sell logic in separate functions,
-# triggering signals if all conditions evaluate to true.
+# print(intraday_data)
 
 class Algorithm:
-    def __init__(dataset):
+    def __init__(self, dataset):
         self.dataset = dataset
         self.pos = 0
         self.num = 0
         self.percent_change = []
+        self.sell_price = 0
 
-    def evaluate:
+    def evaluate(self):
         for i in self.dataset.index:
             self.close = self.dataset['4. close'][i]
             self.ema = self.dataset['EMA'][i]
             if self.pos == 0:
-                buy_evaluation(self.close, self.ema)
+                self.buy_evaluation(i)
             elif self.pos == 1:
-                sell_evaluation(self.close, self.ema)
-            if(self.num == self.dataset['4. close'].count() - 1 and self.pos == 1):
+                self.sell_evaluation(i)
+            if((self.num == self.dataset['4. close'].count() - 1 and self.pos == 1) or ()):
                 sell_price = close
                 self.pos = 0
                 print("Selling now at " + str(sell_price))
                 pc = (sell_price / buy_price - 1) * 100
                 self.percent_change.append(pc)
+                self.performance_calculation()
 
             self.num += 1
 
-    def buy_evaluation(close, ema):
-        if(close < (ema * 0.99)):
-            print("UNDERVALUED: Close: " + str(close) + "  EMA: " + str(ema))
-            self.buy_price = close
+    def buy_evaluation(self, i):
+        if(self.close < (self.ema * 0.99)):
+            print("UNDERVALUED: Close: " + str(self.close) + "  EMA: " + str(self.ema))
+            self.buy_price = self.close
             self.pos = 1
             self.dataset['buy'][i] = True
-            print("Buying now at " + str(buy_price))
+            print("Buying now at " + str(self.buy_price))
 
-    def sell_evaluation(close, ema):
-        if(close > (ema * 1.01)):
-            print("OVERVALUED: Close: " + str(close) + "  EMA: " + str(ema))
-            self.sell_price = close
-            self.pos = 0
-            self.dataset['sell'][i] = True
-            self.pc = (sell_price / buy_price - 1) * 100
-            self.percent_change.append(self.pc)
-            print("Selling now at " + str(self.sell_price) + " , percent change of " + str(self.pc))
+    def sell_evaluation(self, i):
+        if(self.close > (self.ema * 1.01)):
+            print("OVERVALUED: Close: " + str(self.close) + "  EMA: " + str(self.ema))
+            self.sell(i)
+        elif(self.stop_loss(i) == True):
+            print("STOP LOSS: Close: " + str(self.close) + "  EMA: " + str(self.ema))
+            self.sell(i)
 
-    def performance_calculation:
+    def stop_loss(self, i):
+        self.pc = (self.close / self.buy_price - 1) * 100
+        if self.pc < -3:
+            return(True)
+
+    def sell(self, i):
+        self.sell_price = self.close
+        self.pos = 0
+        self.dataset['sell'][i] = True
+        self.pc = (self.sell_price / self.buy_price - 1) * 100
+        self.percent_change.append(self.pc)
+        print("Selling now at " + str(self.sell_price) + " , percent change of " + str(self.pc))
+
+
+    def performance_calculation(self):
         self.gains = 0
         self.gains_count = 0
         self.losses = 0
@@ -63,53 +71,57 @@ class Algorithm:
         self.total_return = 1
         print(self.percent_change)
 
+        for i in self.percent_change:
+            if (i > 0):
+                self.gains += i
+                self.gains_count += 1
+            else:
+                self.losses += i
+                self.losses_count += 1
+            self.total_return = self.total_return * ((i / 100) + 1)
 
+        self.total_return = round((self.total_return - 1) * 100, 2)
 
+        if(self.gains_count > 0):
+            self.average_gain = self.gains / self.gains_count
+            self.max_return = str(max(self.percent_change))
+        else:
+            self.average_gain = 0
+            self.max_return = "none"
 
+        if(self.losses_count > 0):
+            self.average_loss = self.losses / self.losses_count
+            self.max_loss = str(min(self.percent_change))
+            self.ratio = str(-(self.average_gain) / self.average_loss)
+        else:
+            self.average_loss = 0
+            self.max_loss = "none"
+            self.ratio = "infinite"
 
+        print()
+        print("Test period starting at " + str(self.dataset.index[0]) + " for a total of " + str(self.gains_count + self.losses_count) + " trades.")
+        print("Gain/Loss Ratio: " + str(self.ratio))
+        print("Average Gain: " + str(self.average_gain))
+        print("Average Loss: " + str(self.average_loss))
+        print("Max Return: " + str(self.max_return))
+        print("Max Loss: " + str(self.max_loss))
+        print("TOTAL RETURN OVER " + str(self.gains_count + self.losses_count) + " TRADES: " + str(self.total_return) + "%")
+        print()
+        print()
 
-for i in self.percent_change:
-    if (i > 0):
-        self.gains += i
-        self.gains_count += 1
-    else:
-        self.losses += i
-        self.losses_count += 1
-    self.total_return = self.total_return * ((i / 100) + 1)
+algo = Algorithm(intraday_data)
 
-total_return = round((total_return - 1) * 100, 2)
+algo.evaluate()
 
-if(gains_count > 0):
-    self.average_gain = self.gains / self.gains_count
-    self.max_return = str(max(self.percent_change))
-else:
-    self.average_gain = 0
-    self.max_return = "none"
+algo.performance_calculation()
 
-if(losses_count > 0):
-    self.average_loss = self.losses / self.losses_count
-    self.max_loss = str(min(self.percent_change))
-    self.ratio = str(-(self.average_gain) / self.average_loss)
-else:
-    self.average_loss = 0
-    self.max_loss = "none"
-    self.ratio = "infinite"
+evaluated_data = algo.dataset
 
+# with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+#     print(evaluated_data)
 
-print()
-print("Test period starting at " + str(self.dataset.index[0]) + " for a total of " + str(self.gains_count + self.losses_count) + " trades.")
-print("Gain/Loss Ratio: " + str(self.ratio))
-print("Average Gain: " + str(self.average_gain))
-print("Average Loss: " + str(self.average_loss))
-print("Max Return: " + str(self.max_return))
-print("Max Loss: " + str(self.max_loss))
-print("TOTAL RETURN OVER " + str(self.gains_count + self.losses_count) + " TRADES: " + str(self.total_return) + "%")
-print()
-print()
-
-intraday_data.to_json(path_or_buf='intraday/data/data_with_buys.json')
+evaluated_data.to_json(path_or_buf='intraday/data/data_with_buys.json')
 
 print("Records of stock purchases and sales passed back to json")
 
-# with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-#     print(intraday_data)
+
